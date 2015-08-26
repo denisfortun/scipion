f7236e265a50b3ae4334261b6f428bf87301f02c
Denis Fortun <dfortun2@gmail.com>
2015-08-26 11:48:09 +0200

    update protocol_convolution3D

--- pyworkflow/em/packages/bigfluo/protocol_convolution3D.py
+++ pyworkflow/em/packages/bigfluo/protocol_convolution3D.py
@@ -58,33 +58,20 @@ class ProtConvolution3D(EMProtocol):
     
     #--------------------------- STEPS functions --------------------------------------------
     def createOutputStep(self):
-        # Temporary solution specific to my machine
-        # Command lines options of matlab don't work if I don't use directly the binary file in the Matlab folder
-        fnMatlab = "/home/big/public_bin/MATLAB/R2011a/bin/glnxa64/MATLAB"
-        
         fluoVol = self.inputFluo3D.get()
         fnFluoVol = fluoVol.getFileName()
         psf = self.inputPSF.get()
         fnPsf = psf.getFileName()
         fnRoot = self._getExtraPath(pwutils.removeBaseExt(fnFluoVol))
-        # Get the absolute path here
-        fnProject = "/home/big/ScipionUserData/projects/Fluorescence_3D_2/"
-
+   
         from pyworkflow.em.packages.xmipp3 import getMatlabEnviron
-#        args='''-nodesktop -r "diary('%s'); bigfluo_convolution3D('%s','%s','%s'); exit"'''%(fnProject+fnRoot+"_matlab.log",fnProject+fnFluoVol,fnProject+fnPsf,fnProject+fnRoot)
-#        args=''' -nodesktop -r "cd /home/big/Documents/Denis/scipion/matlab_code/test; test_convolution3D; exit"'''
-        args=''' -nodesktop ;exit"'''
-        print(args)
-        print("########  launch matlab") 
-        self.runJob("%s " % fnMatlab, args, env=getMatlabEnviron())
-        print("########  exit matlab") 
-        #/home/big/public_bin/MATLAB/R2011a/bin/glnxa64/MATLAB -nodesktop -r "cd /home/big/Documents/Denis/scipion/matlab_code/test; test_convolution3D;exit"
+        args=''' -r "diary('%s'); bigfluo_convolution3D('%s','%s','%s'); exit"'''%(fnRoot+"_matlab.log",fnFluoVol,fnPsf,fnRoot)
+        self.runJob("matlab", args, env=getMatlabEnviron())
 
         dimx = fluoVol.getDim()[0]
         dimy = fluoVol.getDim()[1]
         dimz = fluoVol.getDim()[2]
         
-        #self.runJob("xmipp_image_convert", "-i %s_convolved.raw#%d,%d,%d,0,float -o %s_convolved.vol" % (fnRoot,dimx,dimy,dimz,fnRoot),env=xmipp3.getEnviron())
         fnOut = "%s_convolved.vol" % fnRoot
         ih = ImageHandler()
         ih.convert("%s_convolved.raw#%d,%d,%d,0,float" % (fnRoot,dimx,dimy,dimz,), fnOut)
@@ -92,5 +79,5 @@ class ProtConvolution3D(EMProtocol):
         outputFluoVol = Volume()
         outputFluoVol.setFileName(fnOut)
         
-        self._defineOutputs(outputFluo3D=outFluoVol)
+        self._defineOutputs(outputFluo3D=outputFluoVol)
 
